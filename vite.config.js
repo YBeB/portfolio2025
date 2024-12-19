51f4ed54bbd802fa755a187c37c4668465634c22
ybeb <younes.bouchi@gmail.com>
2024-12-19 21:13:09 +0100

    majgbt

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/portfolio2025/', // Remplace par le nom de ton dépôt
   plugins: [react()],
-})
+});
