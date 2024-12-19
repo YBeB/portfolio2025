61a9985413e6cc7aba420f332fa022d740ffd503
ybeb <younes.bouchi@gmail.com>
2024-12-19 21:23:53 +0100

    maj

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/portfolio2025/', // Remplace par le nom de ton dépôt
+  base: '/portfolio2025/',
   plugins: [react()],
 });
